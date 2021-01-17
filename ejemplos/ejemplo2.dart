ec13d996170bcb025a54b6e8e4a577430a531522
Daniel <daniel.romero0703@gmail.com>
2021-01-17 01:26:51 -0600

    tercer commit

---
+++ ejemplos/ejemplo2.dart
@@ -0,0 +1,13 @@
+void main(){
+
+int numero =3;
+print ('El valor de la variable es: $numero');
+
+int num2=numero<<1;
+  print('El valor de num2 es: $num2');
+  
+ int num3=numero>>1;
+  print('El valor de num3 es: $num3');
+  
+
+}
